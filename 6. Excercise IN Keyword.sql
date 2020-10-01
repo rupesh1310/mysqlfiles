343df43148f13f21cef926d016e9022a6106ee78
rupesh1310 <a.rupesh66@gmail.com>
2020-10-01 06:23:57 +0530

    :card_file_box: new files

--- 6. Excercise IN Keyword.sql
+++ 6. Excercise IN Keyword.sql
@@ -5,9 +5,9 @@
 */
 
 
-SELECT COUNT(orderid)
-FROM orders
-WHERE customerid IN (7888, 1082, 12808, 9623);
+-- SELECT COUNT(orderid)
+-- FROM orders
+-- WHERE customerid IN (7888, 1082, 12808, 9623);
 
 
 /*
