79d57216686d5fb9dc612007a551427bf886de37
rupesh1310 <a.rupesh66@gmail.com>
2020-10-18 07:49:58 +0530

    :card_file_box: new files

--- 11. Excercise Inner-joins.sql
+++ 11. Excercise Inner-joins.sql
@@ -4,3 +4,34 @@
 * Question: Get all orders from customers who live in Ohio (OH), New York (NY) or Oregon (OR) state
 * ordered by orderid
 */
+
+SELECT c.firstname, c.lastname, o.orderid FROM orders AS o
+INNER JOIN customers AS c ON o.customerid
+WHERE c.statE IN('NY', 'OH', 'OR')
+ORDER BY o.orderid;
+
+
+
+/*
+* DB: Store
+* Table: products
+* Question: Show me the inventory for each product
+*/
+
+SELECT p.prod_id, i.quan_in_stock
+FROM products AS p
+INNER JOIN inventory AS i ON p.prod_id = i.prod_id;
+
+
+
+
+/*
+* DB: Employees
+* Table: employees
+* Question: Show me for each employee which department they work in
+*/
+
+SELECT e.first_name, dp.depart_name
+FROM employees AS e
+INNER JOIN dept_emp AS de ON de.emp_no = e.emp_no
+INNER JOIN departments AS dp ON dp.dept_no = de.dept_no;
