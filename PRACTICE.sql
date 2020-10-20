efbc2058a8926a665d7089316056233d803373aa
rupesh1310 <a.rupesh66@gmail.com>
2020-10-20 08:24:17 +0530

    :card_file_box: new files

--- PRACTICE.sql
+++ PRACTICE.sql
@@ -835,5 +835,24 @@ ANS- --CREATING A VIEW
 
 61. USING VIEWS
 
-ANS- --CURRENT SALARY
-	 
+ANS- 
+	--CURRENT SALARY
+
+	 -- SELECT * FROM last_salary_change
+	 -- SELECT * FROM salaries
+	 -- JOIN last_salary_change AS l USING(emp_no)
+	 -- WHERE from_date = l.max 
+	 -- ORDER BY emp_no
+
+	 CREATE OR REPLACE VIEW last_salary_change AS 
+	 SELECT e.emp_no,
+	 		MAX(s.from_date)
+	 FROM salaries AS s
+
+	 JOIN employees AS e USING(emp_no)
+	 JOIN dept_emp AS de USING(emp_no)
+	 JOIN departments AS d USING(dept_no)
+
+	 GROUP BY e.emp_no
+	 ORDER BY e.emp_no;
+
