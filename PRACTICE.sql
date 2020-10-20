ad766a713cb58dd236a13bd4d545104e9bbfd9d6
rupesh1310 <a.rupesh66@gmail.com>
2020-10-20 06:42:27 +0530

    :card_file_box: new files

--- PRACTICE.sql
+++ PRACTICE.sql
@@ -619,3 +619,28 @@ ANS- --When using a frame clause in a window function we can
 	 			
 	 		)
 	 FROM salaries
+
+
+
+53. SOLVING CURRENT SALARY MYSTERY
+
+ANS- --SOLVING USING WINDOW FUNCTION
+	 SELECT DISTINCT e.emp_no,
+	 	    e.first_name,
+	 	    d.dept_name,
+	 	    LAST_VALUE(s.salary) OVER(
+	 	    	PARTITION BY e.emp_no
+	 	    	ORDER BY s.from_date
+	 	    	RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+	 	    	) AS "Current Salary"
+	 FROM salaries AS s
+
+	 JOIN employees AS a USING(emp_no)
+	 JOIN dept_emp AS de USING(emp_no)
+	 JOIN departments AS d USING(dept_no)
+
+	 ORDER BY e.emp_no;
+
+
+
+54. 
