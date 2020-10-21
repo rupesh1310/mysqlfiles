5b40c5cfe0c5a7e69fe204df00b5aeaccdc2bd04
rupesh1310 <a.rupesh66@gmail.com>
2020-10-21 11:21:47 +0530

    :card_file_box: added files

--- PRACTICE.sql
+++ PRACTICE.sql
@@ -1108,3 +1108,13 @@ ANS- --EXISTS
 
 
 
+
+
+--- student database
+CREATE TABLE student(
+	student_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+	first_name VARCHAR(255) NOT NULL,
+	last_name VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	date_of_birth DATE NOT NULL
+);
