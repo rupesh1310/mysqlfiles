b4b627a08c43c5497b8a905d59b5d16f0140559b
rupesh1310 <a.rupesh66@gmail.com>
2020-10-21 22:09:32 +0530

    :card_file_box: added files

--- PRACTICE.sql
+++ PRACTICE.sql
@@ -1118,3 +1118,15 @@ CREATE TABLE student(
 	email VARCHAR(255) NOT NULL,
 	date_of_birth DATE NOT NULL
 );
+
+
+
+--TABLE CONSTRAINT
+CREATE TABLE student (
+	student_id UUID DEFAULT uuid_generate_v4(),
+	first_name VARCHAR(255) NOT NULL,
+	last_name VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	date_of_birth DATE NOT NULL,
+	CONSTRAINT pk_student_id PRIMARY_KEY (student_id)	
+);
