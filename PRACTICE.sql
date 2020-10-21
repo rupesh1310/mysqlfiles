cf556919d44e84aff6d97a32b561c4562c6895d9
rupesh1310 <a.rupesh66@gmail.com>
2020-10-21 23:00:19 +0530

    :card_file_box: added files

--- PRACTICE.sql
+++ PRACTICE.sql
@@ -1167,3 +1167,34 @@ CREATE TABLE teacher (
 	date_of_birth DATE NOT NULL,
 	email TEXT
 );
+
+
+
+
+ALTER TABLE student
+ADD column email TEXT;
+
+
+
+--CREATE TABLE COURSE
+CREATE TABLE course (
+	course_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+	"name" TEXT NOT NULL,
+	description TEXT,
+	subject_id UUID REFERENCES subject(subject_id),
+	teacher_id UUID REFERENCES teacher(teacher_id),
+	feedback feedback[]
+);
+
+
+
+
+
+--ENROLLMENT
+
+CREATE TABLE enrollment (
+	course_id UUID REFERENCES course(course_id),
+	student_id UUID REFERENCES student(student_id),
+	enrollment_date DATE NOT NULL,
+	CONSTRAINT pk_enrollment PRIMARY KEY (course_id, student_id)
+);
