d2cb2f4d7e7610185a3d8e12e0a4a6ef7577f60e
rupesh1310 <a.rupesh66@gmail.com>
2020-10-22 08:00:45 +0530

    :mag: added files

--- Solving a mystery.sql
+++ Solving a mystery.sql
@@ -15,3 +15,9 @@ FROM suspected_rides AS sr
 JOIN rides AS r ON r.id = sr.ride_id
 JOIN vehicles AS v ON v.id = r.vehicle_id
 JOIN users AS u ON u.id = v.owner_id
+
+
+SELECT DISTINCT r.vehicle_id, u.name AS "rider name", u.address
+FROM suspected_rides AS sr
+JOIN rides AS r ON r.id = sr.ride_id
+JOIN users AS u ON u.id = r.rider_id
